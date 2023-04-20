a2e145a2868ca786b1524cbaf179b6986d864bed
Janssen Kuhn <noconsulate@gmail.com>
2023-04-20 10:06:58 -0600

    don't install if no token

--- src/components/Install.vue
+++ src/components/Install.vue
@@ -4,6 +4,7 @@ import { useRoute } from 'vue-router'
 
 const route = useRoute()
 const { token } = route.query
+const noToken = ref(false)
 
 const requestString = `${import.meta.env.VITE_MONDAY_OAUTH_URL}/authorize?client_id=${import.meta.env.VITE_MONDAY_CLIENT_ID}&state=${token}&redirect_uri=${import.meta.env.VITE_MONDAY_REDIRECT}`
 console.log(requestString)
@@ -13,13 +14,20 @@ const install = () => {
 }
 
 onMounted(() => {
+  console.log(import.meta.env.VITE_MONDAY_REDIRECT)
   // do monday OAuth as user via vizzy server where it builds Vizzy column
-
+  if (!token) {
+    noToken.value = true
+    return
+  }
 
   window.location.href = requestString
 })
 </script>
 
 <template>
-  Installing vizzy...
+  <div>Installing vizzy...</div>
+  <br />
+  <div v-if="noToken">Please go to "Integrations Center" on your <a href="https://www.monday.com">monday.com</a> workspace
+    to install Vizzy to your board.</div>
 </template>
